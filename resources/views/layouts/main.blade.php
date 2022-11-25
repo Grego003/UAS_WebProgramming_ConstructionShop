f9f5bce892afc2b47694f3e98074a2897271844e
ChickenSoup <99463068+Grego003@users.noreply.github.com>
2022-11-25 08:17:26 +0700

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <header>
+        @include('layouts.partials.head')
+    </header>
+</head>
+
+<body>
+
+    @include('layouts.partials.navbar')
+
+    @yield('content')
+
+    @include('layouts.partials.footer')
+
+    @include('layouts.partials.script')
+
+</body>
+
+</html>
---
+++ resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Construction Retail App</title>
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<!-- Font Awesome CSS -->
+
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="{{ asset('css/carousel.css') }}">
