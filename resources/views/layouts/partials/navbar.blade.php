e5262042a158aa600de6c49a236755dd0d3cbeec
agustinusss <agus1natius@gmail.com>
2022-12-01 14:10:03 +0700

    just placeholder to save to branch

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -20,7 +20,26 @@
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
+<<<<<<< Updated upstream
             </form>
+=======
+            </form> --}}
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-1 sm:block">
+                    @auth
+                    <form action="logout" method="POST" class="ms-auto">
+                        @csrf
+                            <button type="submit" class="btn btn-warning">Logout</button>
+                    </form>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm btn btn-light text-decoration-none">Log in</a>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="text-sm btn btn-light text-decoration-none">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+>>>>>>> Stashed changes
         </div>
     </div>
 </nav>
